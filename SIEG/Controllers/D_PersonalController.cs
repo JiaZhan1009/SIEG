cf156100f77f9c3597e5fcca13af177cfa35804b
子祥 <zxc067531@gmail.com>
2023-01-18 16:09:29 +0800

    gradepage

--- SIEG/Controllers/D_PersonalController.cs
+++ SIEG/Controllers/D_PersonalController.cs
@@ -25,6 +25,7 @@ namespace SIEG.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

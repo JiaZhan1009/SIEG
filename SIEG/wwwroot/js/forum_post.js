1e17268ccbd1deb9f7512e43e0565e0c562a1208
GuanYu <q2859640@gmail.com>
2023-02-01 11:07:39 +0800

    文章篩選OK，看板分類做一半

--- SIEG/wwwroot/js/forum_post.js
+++ SIEG/wwwroot/js/forum_post.js
@@ -52,27 +52,5 @@ $('document').ready(function () {
 
 });
 
-/*文章頁發文鈕*/
-let open = '@Url.Action("Forum_post", "Forum")';
-var animateButton = function (e) {
-
-    e.preventDefault;
-    e.target.classList.remove('animate');
-
-    e.target.classList.add('animate');
-
-
-    setTimeout(function () {
-        e.target.classList.remove('animate');
-        window.open('Forum_post');
-    }, 500);
-};
-
-var bubblyButtons = document.getElementsByClassName("bubbly-button");
-
-for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-}
-
 
 

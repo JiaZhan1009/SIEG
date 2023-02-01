150b2d5c5d89fc023e624c9e7747871374cf23c8
BenWang <q0981759218@gmail.com>
2023-02-02 02:27:48 +0800

    會員認證完成

--- SIEG_API/DTO/B_KyccertifiedDTO.cs
+++ SIEG_API/DTO/B_KyccertifiedDTO.cs
@@ -3,7 +3,7 @@
     public class B_KyccertifiedDTO
     {
         public int MemberId { get; set; }
-        public string? IdCardFront { get; set; }
+         public string? IdCardFront { get; set; }
         public string? IdCardBack { get; set; }
         public string? Access { get; set; }
     }
